3833e3c397bf92619e807eb1e23e8dc8932e48c9
Injae <shdlswo7@gmail.com>
2019-01-22 18:17:51 +0900

    깃 테스트

--- knockknock/KnockKnock/src/main/java/com/knockknock/config/SecurityConfig.java
+++ knockknock/KnockKnock/src/main/java/com/knockknock/config/SecurityConfig.java
@@ -9,5 +9,6 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter{
 	@Override
 	public void configure(WebSecurity web) throws Exception{
 		web.ignoring().antMatchers("/**");
+		test
 	}
 }
