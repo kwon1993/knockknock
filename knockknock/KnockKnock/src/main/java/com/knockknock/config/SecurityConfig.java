c0fb1683dca57b89793b683a84f80c7964d887aa
Injae <shdlswo7@gmail.com>
2019-01-22 17:59:36 +0900

    시큐리티디펜던시 추가

--- knockknock/KnockKnock/src/main/java/com/knockknock/config/SecurityConfig.java
+++ knockknock/KnockKnock/src/main/java/com/knockknock/config/SecurityConfig.java
@@ -1,5 +1,13 @@
 package com.knockknock.config;
 
-public class SecurityConfig {
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
+@Configuration
+public class SecurityConfig extends WebSecurityConfigurerAdapter{
+	@Override
+	public void configure(WebSecurity web) throws Exception{
+		web.ignoring().antMatchers("/**");
+	}
 }
