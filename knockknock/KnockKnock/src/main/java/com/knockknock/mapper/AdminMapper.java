e0e0555d5ab74cd0f51de193f40d1234cf3f17fe
kwon1993 <wdmono@naver.com>
2019-01-30 12:39:53 +0900

    관리자 이벤트 crud 진행중

--- knockknock/KnockKnock/src/main/java/com/knockknock/mapper/AdminMapper.java
+++ knockknock/KnockKnock/src/main/java/com/knockknock/mapper/AdminMapper.java
@@ -1,11 +1,13 @@
 package com.knockknock.mapper;
 
+import java.sql.Date;
 import java.util.ArrayList;
 
 import org.apache.ibatis.annotations.Mapper;
 
 import com.knockknock.dto.branch.BranchDTO;
 import com.knockknock.dto.branch.RoomDTO;
+import com.knockknock.dto.event.EventDTO;
 import com.knockknock.dto.event.EventVDTO;
 
 @Mapper
@@ -18,11 +20,12 @@ public interface AdminMapper {
 	public void roomDelete();
 	
 	public ArrayList<EventVDTO> eventListView();
-	public void eventView();
-	public void eventWriteView();
-	public void eventWrite();
-	public void eventModifyView();
-	public void eventModify();
+	public EventVDTO eventView(int writingNumber);
+	public void eventViewHit(int writingNumber);
+	public void eventWrite(int memberNumber, String title, String content, Date eventStartTime, Date eventEndTime, Date acceptStartTime, Date acceptEndTime, int recruitNumber);
+	public ArrayList<Integer> eventWriteNumber(int memberNumber, String title, String content, Date eventStartTime, Date eventEndTime, Date acceptStartTime, Date acceptEndTime, int recruitNumber);
+	public EventDTO eventModifyView(int writingNumber);
+	public void eventModify(int memberNumber, String title, String content, Date eventStartTime, Date eventEndTime, Date acceptStartTime, Date acceptEndTime, int recruitNumber);
 	public void eventDelete();
 	
 }
