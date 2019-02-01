85a5b58ac6bf34bfa4a82181fb45b659075053b4
poussecafe <eatthefish@hanmail.net>
2019-02-01 10:20:43 +0900

    특별한 수정사항 없음(190201, KMH)

--- knockknock/KnockKnock/src/main/java/com/knockknock/dto/branch/ReviewDTO.java
+++ knockknock/KnockKnock/src/main/java/com/knockknock/dto/branch/ReviewDTO.java
@@ -1,9 +1,15 @@
 package com.knockknock.dto.branch;
 
 import java.sql.Date;
-import lombok.Data;
 
-@Data
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+
 public class ReviewDTO {
 	
 	private int writingNumber;
