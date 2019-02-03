a354104cac673805bcc1f9b26774f04922d421cb
BomCoding <masterbmc1@gmail.com>
2019-02-03 22:39:57 +0900

    수정 : 참가하기 수정. DB동작 잘됨 미흡한 부분 : 참가하기 클릭시 현재는 컨트롤러에서 리스트로 밖에 이동못함

--- knockknock/KnockKnock/src/main/java/com/knockknock/dto/event/MeetingVDTO.java
+++ knockknock/KnockKnock/src/main/java/com/knockknock/dto/event/MeetingVDTO.java
@@ -13,6 +13,7 @@ public class MeetingVDTO {
 	private Date acceptStartTime; //모임 모집시작시간
 	private Date acceptEndTime; //모임 모집마감시간
 	private int recruitMaxNumber; //모집인원
+	private int recruitNumber; //모집인원
 	private int hit; //조회수
 	private String simpleIntroduce; //모임 간단소개
 	private String detailIntroduce; //모임 상세소개
