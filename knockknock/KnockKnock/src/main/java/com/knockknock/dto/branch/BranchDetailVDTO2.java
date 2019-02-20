5834beb68c53f4639b2e268c47f0ebe5d976c457
Injae <shdlswo7@gmail.com>
2019-02-20 10:03:22 +0900

    수정 : 쿼리문 수정

--- knockknock/KnockKnock/src/main/java/com/knockknock/dto/branch/BranchDetailVDTO2.java
+++ knockknock/KnockKnock/src/main/java/com/knockknock/dto/branch/BranchDetailVDTO2.java
@@ -4,13 +4,15 @@ import java.io.Serializable;
 import java.util.ArrayList;
 
 import lombok.Data;
-
+//from injae categoryRoomSearch
 @Data
 public class BranchDetailVDTO2 implements Serializable{
 	
 	private static final long serialVersionUID = 1L;
 
-
+	private String branchType;
+	private String isParking;
+	private String isElevator;
 	private String address;
 	private String theme;
 	private String gender;
