8f67655c54bb14c413d26c64af03c4dd1f303a87
BomCoding <masterbmc1@gmail.com>
2019-02-21 12:21:44 +0900

    Revert "수정: 오타수정"
    This reverts commit d1b6ed273139f536c3f6252a7c4c938039f0d9ab.

--- knockknock/KnockKnock/src/main/java/com/knockknock/controller/ReplyController.java
+++ knockknock/KnockKnock/src/main/java/com/knockknock/controller/ReplyController.java
@@ -15,6 +15,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.knockknock.dto.event.MeetingReplyDTO;
+import com.knockknock.dto.event.ReplyDTO;
 import com.knockknock.mapper.ReplyMapper;
 
 @Controller
--- knockknock/KnockKnock/src/main/java/com/knockknock/mapper/ReplyMapper.java
+++ knockknock/KnockKnock/src/main/java/com/knockknock/mapper/ReplyMapper.java
@@ -15,9 +15,9 @@ public interface ReplyMapper {
 	// 미팅 댓글 목록
 	public List<MeetingReplyDTO> meetingReplyList(int writingNumber);
 	//미팅 댓글 작성
-	public int meetingReplyInsert(@Param("meetingReplyDTO") MeetingReplyDTO meetingReplyDTO, @Param("email") String email);
+	public int meetingReplyInsert(@Param("meetingReplyDTO") MeetingReplyDTO meetingReplyDTO, String email);
 	//미팅 댓글수정
-	public int meetingReplyUpdate(MeetingReplyDTO meetingReplyDTO);
+	public int meetingReplyUpdate(@Param("meetingReplyDTO") MeetingReplyDTO meetingReplyDTO);
 	//댓글 삭제
 	public int meetingReplyDelete(int replyNumber);
 	/*
